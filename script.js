7dee271e4c1da862c21221af0a375bb0fac5fa1d
achintyaakumar <achintyakumar5@gmail.com>
2023-03-15 23:36:04 -0600

    Change content on LinkedIn jobs page

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+// const button = document.querySelector('.UserComponent-container');
+// if (button) {
+//   button.insertAdjacentText('afterend', 'hello');
+// }
+
+// Wait until the DOM is loaded
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Select the target node
+//     var targetNode = document.querySelector(".t-black.ph1.t-bold.t-18");
+    
+//     // If the target node exists, add "hello" next to it
+//     if (targetNode) {
+//       var helloNode = document.createElement("span");
+//       helloNode.textContent = "hello";
+//       targetNode.insertAdjacentElement("afterend", helloNode);
+//     } else {
+//       // If the target node does not exist, log an error
+//       console.error("Target node not found!");
+//     }
+    
+//     // Create a new observer
+//     var observer = new MutationObserver(function(mutations) {
+//       mutations.forEach(function(mutation) {
+//         if (mutation.type === "childList") {
+//           // If a child node is added, check if the target node exists
+//           if (mutation.addedNodes.length > 0 && targetNode) {
+//             // If the target node exists, add "hello" next to it
+//             var helloNode = document.createElement("span");
+//             helloNode.textContent = "hello";
+//             targetNode.insertAdjacentElement("afterend", helloNode);
+//           }
+//         }
+//       });
+//     });
+    
+//     // Observe the target node for any changes
+//     observer.observe(document.body, { childList: true, subtree: true });
+//   });
+  
+
+setTimeout(function() {
+    const header = document.querySelector('h2.t-black.ph1.t-bold.t-18');
+    const hello = document.createElement('span');
+    hello.textContent = 'hello';
+    header.insertAdjacentElement('afterend', hello);
+  }, 10000);
+  
